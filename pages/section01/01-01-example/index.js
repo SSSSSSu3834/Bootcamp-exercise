b2d8dd8b7697c5814775939fa044b50bcca1f74b
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-10-12 17:53:14 +0900

    section01

---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,12 @@
+export default function 나만의페이지() {
+  //자바스크림트 작성 공간
+  const a = 3;
+
+  //아래쪽은 html 작성하는 공간
+  return (
+    <div>
+      <div>철수</div>
+      <button>버튼</button>
+    </div>
+  );
+}
